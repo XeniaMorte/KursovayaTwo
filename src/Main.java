aa5dbc7076312250eca7ec9efeedefd6793529b5
xenia <ksen.novikova@mail.ru>
2023-01-08 22:14:48 +0300

    добавлено

--- src/Main.java
+++ src/Main.java
@@ -23,35 +23,39 @@ public class Main {
                 System.out.println("3.Удалить задачу ");
                 System.out.println("4.Выход");
                 choise = scanner.nextInt();
-                if (choise == 1) {
-                    System.out.println("Как назвать задачу?");
-                    String name = scanner.next();
-                    System.out.println("Опишите задачу");
-                    String field = scanner.next();
-                    System.out.println("Какой тип задачи ? Нажмите 1 если личная или 2 если задача рабочая");
-                    int number = scanner.nextInt();
-                    System.out.println(" Выберите повторяемость задачи: " +
-                            "1- однократная,\n" +
-                            "2- ежедневная,\n" +
-                            "3- еженедельная,\n" +
-                            "4- ежемесячная,\n" +
-                            "5- ежегодная.");
+                switch (choise) {
+                    case 1 -> {
+                        System.out.println("Как назвать задачу?");
+                        String name = scanner.next();
+                        System.out.println("Опишите задачу");
+                        String field = scanner.next();
+                        System.out.println("Какой тип задачи ? Нажмите 1 если личная или 2 если задача рабочая");
+                        int number = scanner.nextInt();
+                        System.out.println(" Выберите повторяемость задачи: " +
+                                "1- однократная,\n" +
+                                "2- ежедневная,\n" +
+                                "3- еженедельная,\n" +
+                                "4- ежемесячная,\n" +
+                                "5- ежегодная.");
 
-                    int povtor = scanner.nextInt();
-                    diary.addTask(new Task(name, field, TaskType.getTaskbyNumber(number), LocalDateTime.now(), Utils.getPovtoryaemostById(povtor,LocalDateTime.now().plusHours(1))));
+                        int povtor = scanner.nextInt();
+                        diary.addTask(new Task(name, field, TaskType.getTaskbyNumber(number), LocalDateTime.now(), Utils.getPovtoryaemostById(povtor, LocalDateTime.now().plusHours(1))));
 
-                }
-                if (choise == 2) {
-                    System.out.println( diary.getAllTasksByToday());
-                }
-                if (choise == 3) {
-                    System.out.println("Какую  задачу удалить? Введити id задачи на удаление .");
-                    int delitedId;
-                  delitedId=  scanner.nextInt();
-                  diary.deleteTaskById(delitedId);
-                }
-                if (choise == 4) {
-                   return;
+                    }
+                    case 2 -> {
+                        System.out.println(diary.getAllTasksByToday());
+
+                    }
+                    case 3 -> {
+                        System.out.println("Какую  задачу удалить? Введити id задачи на удаление .");
+                        int delitedId;
+                        delitedId = scanner.nextInt();
+                        diary.deleteTaskById(delitedId);
+
+                    }
+                    case 4 -> {
+                        return;
+                    }
                 }
             } catch (RuntimeException ext) {
                 System.out.println("err: " + ext.getMessage());
